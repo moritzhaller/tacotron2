8697671212e6ef36a7dd215a5aa96e1dfcce848d
Moritz Haller <mhaller@brave.com>
2022-07-02 20:01:16 +0200

    Update symbols

--- text/symbols.py
+++ text/symbols.py
@@ -10,10 +10,12 @@ _pad        = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_umlauts = 'ÄäÖöÜüß'
+_merkel_punctuation = '!,.:;? '
+_merkel_umlauts = 'ÄÖÜäöüß'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + list(_umlauts) + _arpabet
+# symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = list(_special) + list(_merkel_punctuation) + list(_merkel_umlauts) + list(_letters)
