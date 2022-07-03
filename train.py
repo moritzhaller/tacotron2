6ffa18ab6b3cea878be3cf83baa778eb3aa2c7ab
Moritz Haller <mhaller@brave.com>
2022-07-03 15:03:20 +0200

    Update hparams and improve logging

--- train.py
+++ train.py
@@ -234,9 +234,7 @@ def train(output_directory, log_directory, checkpoint_path, warm_start, n_gpus,
     is_overflow = False
     # ================ MAIN TRAINNIG LOOP! ===================
     for epoch in range(epoch_offset, hparams.epochs):
-        print("Epoch: {}".format(epoch))
         for i, batch in enumerate(train_loader):
-            print("Iteration: {}".format(iteration))
             start = time.perf_counter()
             for param_group in optimizer.param_groups:
                 param_group['lr'] = learning_rate
@@ -268,8 +266,8 @@ def train(output_directory, log_directory, checkpoint_path, warm_start, n_gpus,
 
             if not is_overflow and rank == 0:
                 duration = time.perf_counter() - start
-                print("Train loss {} {:.6f} Grad Norm {:.6f} {:.2f}s/it".format(
-                    iteration, reduced_loss, grad_norm, duration))
+                print("Epoch {} Iteration {} Train loss {:.6f} Duration {:.2f}s/it".format(
+                    epoch, iteration, reduced_loss, duration))
                 logger.log_training(
                     reduced_loss, grad_norm, learning_rate, duration, iteration)
 
