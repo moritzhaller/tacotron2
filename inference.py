73fcf63154a5e7fb00e21dd4c7963b9ee63140e0
Moritz Haller <mhaller@brave.com>
2022-07-04 08:04:27 +0200

    update inference

---
+++ inference.py
@@ -0,0 +1,79 @@
+import matplotlib
+import matplotlib.pylab as plt
+
+import numpy as np
+import torch
+from scipy.io.wavfile import write
+import os
+
+from hparams import create_hparams
+from model import Tacotron2
+from layers import TacotronSTFT, STFT
+from audio_processing import griffin_lim
+from train import load_model
+from text import text_to_sequence
+from denoiser import Denoiser
+
+
+def plot_data(data, figsize=(16, 4)):
+    fig, axes = plt.subplots(1, len(data), figsize=figsize)
+    for i in range(len(data)):
+        axes[i].imshow(data[i], aspect='auto', origin='bottom', 
+                       interpolation='none')
+    plt.show()
+
+
+def save_audio(out_dir, file_name, sampling_rate, audio):
+    audio_path = os.path.join(out_dir, "{}_synthesis.wav".format(file_name))
+    write(audio_path, sampling_rate, audio.astype(np.float32))
+
+
+def load_tacotron2(path, hparams):
+    model = load_model(hparams)
+    model.load_state_dict(torch.load(path)['state_dict'])
+    model.cuda().eval().half()
+
+    return model
+
+
+def load_waveglow(path):
+    waveglow = torch.load(path)['model']
+    waveglow.cuda().eval().half()
+    for k in waveglow.convinv:
+        k.float()
+    denoiser = Denoiser(waveglow)
+
+    return waveglow, denoiser
+
+
+def infer(tacotron2_path, waveglow_path, save=False):
+    hparams = create_hparams()
+    hparams.max_wav_value=32768.0
+    hparams.sampling_rate = 22050
+    hparams.filter_length=1024
+    hparams.hop_length=256
+    hparams.win_length=1024
+    tacotron2 = load_tacotron2(tacotron2_path, hparams)
+    waveglow, denoiser = load_waveglow(waveglow_path)
+
+    text = "Natürlich messe ich dem Lesen eine sehr, sehr große Bedeutung bei!"
+    sequence = np.array(text_to_sequence(text, ['german_cleaners']))[None, :]
+    sequence = torch.autograd.Variable(
+        torch.from_numpy(sequence)).cuda().long()
+
+    # text -> mel spectogram
+    mel_outputs, mel_outputs_postnet, _, alignments = tacotron2.inference(sequence)
+
+    # mel spectogram -> sound wave
+    with torch.no_grad():
+        audio = waveglow.infer(mel_outputs_postnet, sigma=0.85)
+
+    # denoise
+    audio_denoised = denoiser(audio, strength=0.006)[:, 0]
+    
+    audio_denoised_np = audio_denoised.cpu().numpy()    
+    
+    if save:
+        save_audio('foo', 'bar', hparams.sampling_rate, audio_denoised_np)
+
+    return audio_denoised_np
