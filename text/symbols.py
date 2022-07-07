150e61f1507d493d803fd206bceff4ff0631da5f
Moritz Haller <mhaller@brave.com>
2022-07-07 20:00:21 +0200

    fix

--- text/symbols.py
+++ text/symbols.py
@@ -4,18 +4,26 @@
 Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
+from symtable import Symbol
 from text import cmudict
 
 _pad        = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_merkel_punctuation = '!,.:;? '
-_merkel_umlauts = 'ÄÖÜäöüß'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
 # symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
-symbols = list(_special) + list(_merkel_punctuation) + list(_merkel_umlauts) + list(_letters)
+chars = [
+    ' ', '!', ',', '-', '.', ':', ';', '?', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Ä', 'Ö', 'Ü', 'ß', 'ä',
+    'ö', 'ü'
+]
+
+symbols = chars + _arpabet
